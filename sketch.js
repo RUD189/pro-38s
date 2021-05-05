f92636b1c62c4b44a9bf31f158ce31c01aefd632
RUD189 <81680588+RUD189@users.noreply.github.com>
2021-05-05 12:02:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,16 +12,17 @@ var score;
 
 var gameOverImg,restartImg
 var jumpSound , checkPointSound, dieSound
-
-
+var g,gI;
+var back;
+var sun,so;
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadAnimation("trex_collided.png");
-  
+  back = loadImage("backgroundImg.png");
   groundImage = loadImage("ground2.png");
-  
+  sun = loadImage("sun.png");
   cloudImage = loadImage("cloud.png");
-  
+  gI = loadImage("grounds.png");
   obstacle1 = loadImage("obstacle1.png");
   obstacle2 = loadImage("obstacle2.png");
   obstacle3 = loadImage("obstacle3.png");
@@ -40,25 +41,31 @@ function preload(){
 function setup() {
   createCanvas(displayWidth, displayHeight - 150);
   
-  trex = createSprite(displayWidth/4,displayHeight-500,20,50);
+  trex = createSprite(displayWidth/4,displayHeight/2 + 200,20,50);
+  
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided" ,trex_collided);
-  trex.scale = 0.5;
+  trex.scale = 0.7; 
   
-  ground = createSprite(displayWidth/3,displayHeight-500,width,2);
-  ground.addImage("ground",groundImage);
+
+  ground = createSprite(displayWidth + 300,displayHeight/2+330,width,2);
+  ground.addImage("ground",gI);
+  ground.scale = 0.9;
  
+  so = createSprite(displayWidth/2 + 800,displayHeight/2 - 380);
+  so.addImage(sun);    
+  so.scale = 0.3;
   
-   gameOver = createSprite(width/4,height/2-50);
+   gameOver = createSprite(width/2,height/2-80);
   gameOver.addImage(gameOverImg);
   
-  restart = createSprite(width/4,height/2);
+  restart = createSprite(width/2,height/2);
   restart.addImage(restartImg);
   
-  gameOver.scale = 0.5;
-  restart.scale = 0.5;
+  gameOver.scale = 1;
+  restart.scale = 1;
   
-  invisibleGround = createSprite(displayWidth/2,displayHeight-465,width,40);
+  invisibleGround = createSprite(displayWidth/2,displayHeight/2 + 250,width,40);
   invisibleGround.visible = false;
   
   //create Obstacle and Cloud Groups
@@ -76,13 +83,15 @@ function setup() {
 
 function draw() {
   
-  background(250);
-  //displaying score
-  textStyle(ITALIC);     
+  background(back);
+  //displaying score     
   textSize(20);
-  text("Score: "+ score, displayWidth/2 - 500,displayHeight/2 - 350);
+  text("'USE SPACE BAR TO JUMP'", displayWidth/2 - 900,displayHeight/2 - 510);
+  textSize(25);
+  text("Score: "+ score, displayWidth/2 - 900,displayHeight/2 - 480);
   
-
+  trex.depth = ground.depth;
+  trex.depth = ground.depth + 1;
  
   
   if(gameState === PLAY){
@@ -97,14 +106,14 @@ score = score + Math.round(getFrameRate()/60);
     if(score > 0 && score % 100 === 0){
       checkPointSound.play();
     }
-    
-    if (ground.x < 300){
-      ground.x = displayWidth/3;
+     
+    if (ground.x < displayWidth/6 - 620){ 
+      ground.x = displayWidth;
     }
     
     //jump when the space key is pressed
-    if(touches.length > 0 || keyDown("space") && trex.y >= displayHeight-520) {
-        trex.velocityY = -12.99;
+    if(touches.length > 0 || keyDown("space") && trex.y >= displayHeight/2 + 200) {
+        trex.velocityY = -13.5;
       jumpSound.play();
       touches = [];
     }
@@ -154,9 +163,6 @@ score = score + Math.round(getFrameRate()/60);
   //stop trex from falling down
   trex.collide(invisibleGround);
   
-  camera.x = trex.x;
-  camera.y = trex.y;
-  
   drawSprites();
 }
 
@@ -170,14 +176,14 @@ function reset(){
    cloudsGroup.destroyEach();
   trex.changeAnimation("running",trex_running);
   
-
 }
 
 function spawnObstacles(){
- if (frameCount % 60 === 0){2
-   var obstacle = createSprite(camera.x+width/2, 570,20,30);
-   obstacle.velocityX = ground.velocityX; 
-  
+ if (frameCount % 100 === 0){
+   var obstacle = createSprite(camera.x+width/2 + 30, displayHeight/2 + 50,20,30);
+   obstacle.velocityX = -(6 + 3*score/100); 
+   obstacle.y = invisibleGround.y - 50 ;
+
     //generate random obstacles
     var rand = Math.round(random(1,6));
     switch(rand) {
@@ -197,11 +203,12 @@ function spawnObstacles(){
     }
    
     //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.55;
+    obstacle.scale = 0.7;
     obstacle.lifetime = 300;
    
    //add each obstacle to the group
     obstaclesGroup.add(obstacle);
+    
  }
 }
 
